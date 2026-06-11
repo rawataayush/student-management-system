bd98e9dbe0ca4af1bed338120545ac3d6315821c
Aayush Rawat <aayushrawat.dev@gmail.com>
2026-06-11 21:11:22 +0530

    Create student card component and render student data

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const StudentCard = ({student}) => {
     return (
-        <div className="flex flex-wrap bg-gray-300">
+        <div className="flex flex-wrap m-3.5">
+            <div className="card w-full p-5 rounded bg-gray-100">
+                <h3 className="text-2xl pb-4 text-wrap">{student.name}</h3>
+                <p>Age: {student.age}</p>
+                <p>Course: {student.course}</p>
+            </div>
         </div>
     )
 }
