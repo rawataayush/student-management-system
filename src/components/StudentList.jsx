b3f6bb5e06c1a30e0d40269ec9177dc8d8d9013d
Aayush Rawat <aayushrawat.dev@gmail.com>
2026-06-23 10:47:22 +0530

    Fix empty state layout and alignment

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -5,18 +5,23 @@ const StudentList = ({students, handleDelete}) => {
 
     return (
         <>
-            <h2 className="text-2xl md:text-3xl text-center">Students</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
-                {students.length > 0 ? students.map((student, index)=> (
-                    <StudentCard
-                        key={index}
-                        student={student}
-                        index={index}
-                        handleDelete={handleDelete} />
-                )) : (
-                    <p className="text-gray-400 text-center md:text-xl">No students added yet.</p>
+            <h2 className="text-2xl md:text-3xl text-center mb-4">Students</h2>
+            
+                {students.length > 0 ? (<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'> 
+                    {students.map((student, index)=> (
+                        <StudentCard
+                            key={index}
+                            student={student}
+                            index={index}
+                            handleDelete={handleDelete} />
+                    ))}
+                </div>) : (
+                    <div className='flex items-center justify-center h-40'>
+                        <p className="text-gray-400 text-center md:text-xl">
+                            No students added yet.
+                        </p>
+                    </div>
                 )}
-            </div>
         </>
     )
 }
